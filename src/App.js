add130bb0ad1bc35debe3e4818d3de484d696b63
vibakar <vibakar23@gmail.com>
2019-12-01 17:52:30 +0530

    Lazy loading implemented

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route } from 'react-router-dom';
 import Header from './components/header';
 import Footer from './components/footer';
-import About from './components/about';
-import Home from './components/home';
-import ProductDetail from './components/productDetail';
-import AddProduct from './components/addProduct';
+// import About from './components/about';
+// import Home from './components/home';
+// import ProductDetail from './components/productDetail';
+// import AddProduct from './components/addProduct';
+
+const Home = lazy(() => import('./components/home'));
+const ProductDetail = lazy(() => import('./components/productDetail'));
+const AddProduct = lazy(() => import('./components/addProduct'));
+const About = lazy(() => import('./components/about'));
 
 const App = () => {
   return (
     <div>
       <Header></Header>
       <div className="mt-56">
-        <Route path="/" exact component={Home} />
-        <Route path="/productDetail/:id" component={ProductDetail} />
-        <Route path="/add-product" component={AddProduct} />
-        <Route path="/edit-product/:id" component={AddProduct} />
-      	<Route path="/about" component={About} />
+        <Suspense fallback={<h3>Loading...</h3>}>
+          <Route path="/" exact component={Home} />
+          <Route path="/productDetail/:id" component={ProductDetail} />
+          <Route path="/add-product" component={AddProduct} />
+          <Route path="/edit-product/:id" component={AddProduct} />
+        	<Route path="/about" component={About} />
+        </Suspense>
       </div>
       <Footer></Footer>
     </div>
